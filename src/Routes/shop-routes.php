b86f1d7137c135b4e71640c74efc2931ea6ffdd9
Marco Alexis Zacarias Rubio <marcoalexiszacariasrubio@gmail.com>
2025-05-30 14:25:50 -0600

    Eliminar directorios y archivos innecesarios para el tema

--- src/Routes/shop-routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Goloba\GolobaTheme\Http\Controllers\Shop\GolobaThemeController;
-
-Route::group(['middleware' => ['web', 'theme', 'locale', 'currency'], 'prefix' => 'golobatheme'], function () {
-    Route::get('', [GolobaThemeController::class, 'index'])->name('shop.golobatheme.index');
-});
