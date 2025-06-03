b4913795d6ac3e00ba8800bf675c9895f4ed3658
Marco Alexis Zacarias Rubio <marcoalexiszacariasrubio@gmail.com>
2025-06-03 16:41:59 -0600

    Agregar views y assets del paquete marketplace

---
+++ src/Resources/views/webkul/marketplace/components/shop/layouts/header/sell.blade.php
@@ -0,0 +1,9 @@
+@if (core()->getConfigData('marketplace.settings.general.status'))
+    <a
+        href="{{ route('marketplace.seller_central.index') }}"
+        aria-label="Sell"
+        class="hidden" {{-- Oculto de manera temporal --}}
+    >
+        <span class="mp-store-icon inline-block cursor-pointer text-[24px]"></span>
+    </a>
+@endif
