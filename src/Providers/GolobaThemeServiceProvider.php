707645bbd51563bbed6e93d403b8d7e559320349
Marco Alexis Zacarias Rubio <marcoalexiszacariasrubio@gmail.com>
2025-08-14 23:01:48 +0000

    Modificar GolobaThemeServiceProvider

--- src/Providers/GolobaThemeServiceProvider.php
+++ src/Providers/GolobaThemeServiceProvider.php
@@ -3,43 +3,51 @@
 namespace Webkul\GolobaTheme\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Webkul\Customer\Models\Customer as BaseCustomer;
+use Webkul\GolobaTheme\Models\Customer;
 
 class GolobaThemeServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap services.
-     *
-     * @return void
      */
     public function boot()
     {
-        // Service provider code will be added here
+        // Publica vistas en caso de que se necesiten mover a resources
         $this->publishes([
-        __DIR__.'/../Resources/views'  => resource_path('themes/goloba-theme/views'),
-    ]);
+            __DIR__ . '/../Resources/views' => resource_path('themes/goloba-theme/views'),
+        ]);
 
-    $this->loadVendorViews();
+        // Carga rutas si existen
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
+
+        // Se agregan las traducciones customizadas ubicadas dentro del tema.
+        $this->loadTranslationsFrom(__DIR__ . '/../Resources/assets/marketplace/lang', 'marketplace');
+	    $this->loadTranslationsFrom(__DIR__ . '/../Resources/assets/shop/lang', 'shop');
+	    $this->loadTranslationsFrom(__DIR__ . '/../Resources/assets/admin/lang', 'admin');
+        // Sobrescribe vistas de los paquetes
+        $this->loadVendorViews();
+	    $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
     }
 
-    /**
-     * Load vendor package views override
-     */
     protected function loadVendorViews()
     {
+      	$this->loadViewsFrom(__DIR__.'/../Resources/views', 'goloba-theme');
+    
+        // Sobrescribir marketplace pero apuntando a la estructura correcta
         $this->app['view']->prependNamespace('marketplace', __DIR__ . '/../Resources/views/webkul/marketplace');
+        
+        $this->app['view']->prependNamespace('admin', __DIR__ . '/../Resources/views/webkul/admin');
     }
 
-    /**
-     * Register services.
-     *
-     * @return void
-     */
     public function register()
     {
-        //
         $this->mergeConfigFrom(
-        dirname(__DIR__).'/Config/bagisto-vite.php',
-        'bagisto-vite.viters'
-    );
+            dirname(__DIR__) . '/Config/bagisto-vite.php',
+            'bagisto-vite.viters'
+        );
+
+        // Registrar el modelo Customer personalizado
+        $this->app->bind(BaseCustomer::class, Customer::class);
     }
 }
